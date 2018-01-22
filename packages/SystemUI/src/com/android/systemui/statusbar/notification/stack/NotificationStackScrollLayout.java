625c7e45b4583e5841bc0a960f3feadb42ef69ef
ezio84 <brabus84@gmail.com>
2018-01-22 15:20:58 +0100

    Allow to dismiss notifications with back FP left/right swipe [1/2]
    if the panel is expanded.
    
    The swipe animation will follow the swipe gesture direction.
    
    Change-Id: I358ba07fa7724f9549145c87a1440916c469ca53
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -544,7 +544,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
         mSectionsManager.setOnClearGentleNotifsClickListener(v -> {
             // Leave the shade open if there will be other notifs left over to clear
             final boolean closeShade = !hasActiveClearableNotifications(ROWS_HIGH_PRIORITY);
-            clearNotifications(ROWS_GENTLE, closeShade);
+            clearNotifications(ROWS_GENTLE, closeShade, false/*forceToLeft*/);
         });
 
         mAmbientState = new AmbientState(context, mSectionsManager, mHeadsUpManager);
@@ -1757,9 +1757,9 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
     }
 
     @ShadeViewRefactor(RefactorComponent.STATE_RESOLVER)
-    public void dismissViewAnimated(View child, Runnable endRunnable, int delay, long duration) {
+    public void dismissViewAnimated(View child, Runnable endRunnable, int delay, long duration, boolean forceToLeft) {
         mSwipeHelper.dismissChild(child, 0, endRunnable, delay, true, duration,
-                true /* isDismissAll */);
+                true /* isDismissAll */, forceToLeft);
     }
 
     @ShadeViewRefactor(RefactorComponent.STATE_RESOLVER)
@@ -5454,9 +5454,9 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
     }
 
     @ShadeViewRefactor(RefactorComponent.SHADE_VIEW)
-    private void clearNotifications(
+    public void clearNotifications(
             @SelectedRows int selection,
-            boolean closeShade) {
+            boolean closeShade, boolean forceToLeft) {
         // animate-swipe all dismissable notifications, then animate the shade closed
         int numChildren = getChildCount();
 
@@ -5504,7 +5504,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
             return;
         }
 
-        performDismissAllAnimations(viewsToHide, closeShade, () -> {
+        performDismissAllAnimations(viewsToHide, closeShade, forceToLeft, () -> {
             for (ExpandableNotificationRow rowToRemove : viewsToRemove) {
                 if (StackScrollAlgorithm.canChildBeDismissed(rowToRemove)) {
                     if (selection == ROWS_ALL) {
@@ -5553,6 +5553,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
     private void performDismissAllAnimations(
             final ArrayList<View> hideAnimatedList,
             final boolean closeShade,
+            final boolean forceToLeft,
             final Runnable onAnimationComplete) {
 
         final Runnable onSlideAwayAnimationComplete = () -> {
@@ -5588,7 +5589,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
             if (i == 0) {
                 endRunnable = onSlideAwayAnimationComplete;
             }
-            dismissViewAnimated(view, endRunnable, totalDelay, ANIMATION_DURATION_SWIPE);
+            dismissViewAnimated(view, endRunnable, totalDelay, ANIMATION_DURATION_SWIPE, forceToLeft);
             currentDelay = Math.max(50, currentDelay - rowDelayDecrement);
             totalDelay += currentDelay;
         }
@@ -5601,7 +5602,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
                 R.layout.status_bar_notification_footer, this, false);
         footerView.setDismissButtonClickListener(v -> {
             mMetricsLogger.action(MetricsEvent.ACTION_DISMISS_ALL_NOTES);
-            clearNotifications(ROWS_ALL, true /* closeShade */);
+            clearNotifications(ROWS_ALL, true /* closeShade */, false/*forceToLeft*/);
         });
         footerView.setManageButtonClickListener(this::manageNotifications);
         setFooterView(footerView);
