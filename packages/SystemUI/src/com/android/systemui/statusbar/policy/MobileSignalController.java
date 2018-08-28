631ad35c8361471415e78cc2caeb562248d277f7
Weijie Wang <weijiew@codeaurora.org>
2018-08-28 14:51:58 +0800

    SystemUI: Fix volte icon doesn't update in real time
    SystemUI will clean and reinitialize the subscripiton state
    when active subscripitons are chagned, but telephony doesn't
    report the ims registration state if it is not chagned.
    
    When two sims are both registered on ims, hotswap sim1.
    In this case, the volte icon of sim2 will not be updated in time
    
    Now we query the ims registration state first when reinitialize
    the subscripiton state; we also query the ims registration state
    when receiveing ImsRegistrationImplBase.Callback
    
    Change-Id: Ie12ca7c1e82eb2dcae933d5fb8c93937c5e1a050
    CRs-Fixed: 2304136
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -133,6 +133,7 @@ public class MobileSignalController extends SignalController<
 
         int phoneId = SubscriptionManager.getPhoneId(mSubscriptionInfo.getSubscriptionId());
         mImsManager = ImsManager.getInstance(mContext, phoneId);
+        updateImsRegistrationState();
 
         mObserver = new ContentObserver(new Handler(receiverLooper)) {
             @Override
@@ -206,6 +207,11 @@ public class MobileSignalController extends SignalController<
     public void unregisterListener() {
         mPhone.listen(mPhoneStateListener, 0);
         mContext.getContentResolver().unregisterContentObserver(mObserver);
+        try {
+            mImsManager.removeRegistrationListener(mImsRegistrationCallback);
+        }catch(ImsException e){
+            Log.d(mTag, "exception:" + e);
+        }
         mContext.unregisterReceiver(mVolteSwitchObserver);
     }
 
@@ -393,6 +399,11 @@ public class MobileSignalController extends SignalController<
             resId = R.drawable.ic_volte;
         }
         return resId;
+    }
+
+    private void updateImsRegistrationState() {
+        mImsResitered = mPhone.isImsRegistered(mSubscriptionInfo.getSubscriptionId());
+        Log.d(mTag, "updateImsRegistrationState mImsResitered=" + mImsResitered);
     }
 
     @Override
@@ -721,6 +732,15 @@ public class MobileSignalController extends SignalController<
         public void onActiveDataSubscriptionIdChanged(int subId) {
             if (DEBUG) Log.d(mTag, "onActiveDataSubscriptionIdChanged: subId=" + subId);
             updateDataSim();
+	    updateTelephony();
+	}
+
+	@Override
+        public void onCallStateChanged(int state, String phoneNumber) {
+            if (DEBUG) {
+                Log.d(mTag, "onCallStateChanged: state=" + state);
+            }
+            mCallState = state;
             updateTelephony();
         }
 
@@ -740,7 +760,7 @@ public class MobileSignalController extends SignalController<
                 public void onRegistered(
                         @ImsRegistrationImplBase.ImsRegistrationTech int imsRadioTech) {
                     Log.d(mTag, "onRegistered imsRadioTech=" + imsRadioTech);
-                    mImsResitered = true;
+                    updateImsRegistrationState();
                     notifyListeners();
                 }
 
@@ -748,14 +768,14 @@ public class MobileSignalController extends SignalController<
                 public void onRegistering(
                         @ImsRegistrationImplBase.ImsRegistrationTech int imsRadioTech) {
                     Log.d(mTag, "onRegistering imsRadioTech=" + imsRadioTech);
-                    mImsResitered = false;
+                    updateImsRegistrationState();
                     notifyListeners();
                 }
 
                 @Override
                 public void onDeregistered(ImsReasonInfo imsReasonInfo) {
                     Log.d(mTag, "onDeregistered imsReasonInfo=" + imsReasonInfo);
-                    mImsResitered = false;
+                    updateImsRegistrationState();
                     notifyListeners();
                 }
             };
