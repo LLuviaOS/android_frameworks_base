AOSiP/platform_frameworks_base

base: Revert "Apply front scrim to doze pulsing"
Fixes doze pulse notification

This reverts commit d424e202b361ce1b055367f107f5d87118456ce8.
Change-Id: I208a06cbe9cf87f733932a0248a5a4a3c9ea6cad
Signed-off-by: Lucchetto <lucchetto.tie@live.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -139,7 +139,7 @@ public enum ScrimState {
     PULSING(5) {
         @Override
         public void prepare(ScrimState previousState) {
-            mFrontAlpha = mAodFrontScrimAlpha;
+            mFrontAlpha = 0f;
             mBubbleAlpha = 0f;
             mBehindTint = Color.BLACK;
             mFrontTint = Color.BLACK;

LLuviaOS/android_frameworks_base

Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -127,7 +127,7 @@ public enum ScrimState {
     PULSING(5) {
         @Override
         public void prepare(ScrimState previousState) {
-            mCurrentInFrontAlpha = mAodFrontScrimAlpha;
+            mCurrentInFrontAlpha = 0f;
             mCurrentBehindTint = Color.BLACK;
             mCurrentInFrontTint = Color.BLACK;
             mBlankScreen = mDisplayRequiresBlanking;