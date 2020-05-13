5c65097b8846cfff24bd44cc75993c061898bbd8
Marko Man <darkobas@gmail.com>
2020-05-13 14:24:22 +0000

    overlay: Introduce LLuvia Primary
    Change-Id: Icea780d880b5c38598e5d8dff07134fea8e85e4e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -50,6 +50,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
+        PrimaryColorLLUVIABlackOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
